2a16ce9c4556af19a0892391abbf5a74c2e71401
SADIK KUZU <sadikkuzu@hotmail.com>
2025-03-30 15:01:20 +0300

    Test coingecko

--- tests/acceptance/test_dummy.py
+++
@@ -1,6 +0,0 @@
-from btctry.utils import get_btctry, prepare_for_put  # noqa: F401
-
-
-# This is a dummy test to ensure that the test suite runs without errors.
-def test_anonymous():
-    assert True
